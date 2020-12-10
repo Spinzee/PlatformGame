90ed8564e0a9a412d87e38392c99cde3e69d5923
Lindsey Wilson <Lindsey.donaldson@live.com>
2020-12-10 11:48:57 +0000

    add  text animation on menu

---
+++ Assets/Scripts/Menu Scripts/GoOffScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GoOffScreen : MonoBehaviour
+{
+    public List<GameObject> MoveObjects;
+
+    public void MoveOffScreen()
+    {
+        foreach (GameObject obj in MoveObjects)
+        {
+            obj.GetComponent<Animator>().SetBool("onScreen", false);
+        }
+    }
+}
